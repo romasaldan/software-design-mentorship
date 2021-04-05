0bb5f90f72722042cd221092ed055219669b8530
romasaldan <romasaldan@gmail.com>
2021-04-05 19:21:33 +0300

    first task

--- 01_oop_and_ood/src/Point.ts
+++ 01_oop_and_ood/src/Point.ts
@@ -1,1 +1,44 @@
-export class Point {}
+import {SafeFloatService} from './SafeFloatService';
+
+export class Point {
+  public x: number;
+  public y: number;
+  private safeFloatService = new SafeFloatService();
+
+  constructor();
+  constructor(x: number, y: number);
+
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return `(${this.x}, ${this.y})`;
+  }
+
+  distance(other: Point): number;
+  distance(x: number, y: number): number;
+  distance(): number;
+
+  distance(a?: Point | number, b?: number) {
+    if (a instanceof Point) {
+      return this.calculateDistanceToPoint(a);
+    }
+
+    if (typeof a === 'number' && typeof a === 'number') {
+      return this.calculateDistanceToPoint(new Point(a, b));
+    }
+
+    return this.calculateDistanceToPoint(new Point());
+  }
+
+  private calculateDistanceToPoint(point: Point): number {
+    return Math.sqrt(
+      this.safeFloatService.add(
+        this.safeFloatService.substract(this.x, point.x) ** 2,
+        this.safeFloatService.substract(this.y, point.y) ** 2
+      )
+    );
+  }
+}
--- 01_oop_and_ood/src/Shape.ts
+++ 01_oop_and_ood/src/Shape.ts
@@ -1,3 +1,48 @@
+import {Point} from './point';
+
 export abstract class Shape {
-    abstract getType(): string;
+  protected color: string;
+  protected filled: boolean;
+  protected points: Point[];
+  private readonly MIN_NUMBER_OF_POINTS = 3;
+
+  abstract getType(): string;
+
+  constructor(points: Point[]);
+  constructor(points: Point[], color: string, filled: boolean);
+
+  constructor(points: Point[], color = 'green', filled = true) {
+    this.validateInputs(points);
+
+    this.points = points;
+    this.color = color;
+    this.filled = filled;
+  }
+
+  private validateInputs(points: Point[]) {
+    if (points.length < this.MIN_NUMBER_OF_POINTS) {
+      throw Error('should be passed at least three points to form a shape');
+    }
+  }
+
+  toString() {
+    const pointsList = this.points.join(', ');
+    const filledStatus = this.filled ? 'filled' : 'not filled';
+
+    return `A Shape with color of ${this.color} and ${filledStatus}. Points: ${pointsList}.`;
+  }
+
+  public getPerimeter() {
+    return this.getAllDistances().reduce((acc, distance) => acc + distance, 0);
+  }
+
+  protected getAllDistances(): number[] {
+    return this.points.map((point, index) => {
+      if (this.points.length - 1 === index) {
+        return point.distance(this.points[0]);
+      }
+
+      return point.distance(this.points[index + 1]);
+    });
+  }
 }
