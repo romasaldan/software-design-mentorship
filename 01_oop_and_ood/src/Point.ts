abcd6181c4bac7645d77dcecc253d91f7bed628d
Roman Saldan <35783096+romasaldan@users.noreply.github.com>
2021-04-08 22:02:54 +0300

    first module (#1)
    first task

--- 01_oop_and_ood/src/Point.ts
+++ 01_oop_and_ood/src/Point.ts
@@ -1,1 +1,41 @@
-export class Point {}
+import {SafeFloatService} from './SafeFloatService';
+
+export class Point {
+  public x: number;
+  public y: number;
+  private safeFloatService = new SafeFloatService();
+
+  constructor(x: number = 0, y: number = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return `(${this.x}, ${this.y})`;
+  }
+
+  distance(other: Point): number;
+  distance(x: number, y: number): number;
+  distance(): number;
+
+  distance(a?: Point | number, b?: number) {
+    if (a instanceof Point) {
+      return this.calculateDistanceToPoint(a);
+    }
+
+    if (typeof a === 'number') {
+      return this.calculateDistanceToPoint(new Point(a, b));
+    }
+
+    return this.calculateDistanceToPoint(new Point());
+  }
+
+  private calculateDistanceToPoint(point: Point): number {
+    return Math.sqrt(
+      this.safeFloatService.add(
+        this.safeFloatService.substract(this.x, point.x) ** 2,
+        this.safeFloatService.substract(this.y, point.y) ** 2
+      )
+    );
+  }
+}
--- 01_oop_and_ood/src/Triangle.ts
+++ 01_oop_and_ood/src/Triangle.ts
@@ -1,3 +1,46 @@
-import { Shape } from './Shape';
+import {Point} from './point';
+import {Shape} from './Shape';
 
-export class Triangle extends Shape {}
+export class Triangle extends Shape {
+  private readonly DECIMAL_PRECISION = 3;
+
+  constructor(v1: Point, v2: Point, v3: Point);
+  constructor(v1: Point, v2: Point, v3: Point, color: string, filled: boolean);
+
+  constructor(v1, v2, v3, color?: string, filled?: boolean) {
+    super([v1, v2, v3], color, filled);
+  }
+
+  toString() {
+    const triangleDescription = this.points
+      .map((point, i) => `v${i + 1}=${point}`)
+      .join(',');
+
+    return `Triangle[${triangleDescription}]`;
+  }
+
+  getType() {
+    const distances = new Set(this.getDistancesWithPresice());
+
+    switch (distances.size) {
+      case 1:
+        return 'equilateral triangle';
+      case 2:
+        return 'isosceles triangle';
+      case 3:
+        return 'scalene triangle';
+      default:
+        throw Error(
+          'something wrong with your code, the number of distances can not be more than three for a triangle'
+        );
+    }
+  }
+
+  private getDistancesWithPresice() {
+    return this.getAllDistances().map(
+      (distance) =>
+        Math.round(distance * 10 ** this.DECIMAL_PRECISION) /
+        1000 ** this.DECIMAL_PRECISION
+    );
+  }
+}
