46964f97ba24bf37ed53f330bcde84699f97b242
Roman Saldan <Roman_Saldan@epam.com>
2021-04-09 22:53:00 +0300

    part of task

--- 02_oop_part_1/code/src/Item.ts
+++ 02_oop_part_1/code/src/Item.ts
@@ -1,11 +1,65 @@
-import { Comparable } from './Comparable';
-
-let id = 0;
+import {Comparable} from './Comparable';
 
 export abstract class Item implements Comparable<Item> {
-    public compareTo(other: Item): number {
-        // your code goes here
+  private static instancesAmount = 0;
+  private readonly PRECISION = 2;
+  private id: number;
+  private value: number;
+  private weight: number;
+  private name: string;
+
+  constructor() {
+    this.id = Item.instancesAmount;
+    Item.instancesAmount++;
+  }
+
+  public compareTo(other: Item): number {
+    if (this.value === other.getValue()) {
+      return this.name
+        .toLowerCase()
+        .localeCompare(other.getName().toLowerCase());
     }
 
-    // your code goes here
+    return Math.sign(this.value - other.getValue());
+  }
+
+  protected formatNumber(number: number): string {
+    return String(Math.round(number * this.PRECISION) / this.PRECISION);
+  }
+
+  toString() {
+    return `${this.name} - Value: ${this.formatNumber(
+      this.value
+    )}, Weight: ${this.formatNumber(this.weight)}`;
+  }
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public getValue(): number {
+    return this.value;
+  }
+
+  public getWeight(): number {
+    return this.weight;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setValue(price: number) {
+    this.value = price;
+  }
+
+  public setName(name: string) {
+    this.name = name;
+  }
+
+  public setWeight(weight: number) {
+    this.weight = weight;
+  }
+
+  public reset() {}
 }
--- 02_oop_part_1/code/src/Weapon.ts
+++ 02_oop_part_1/code/src/Weapon.ts
@@ -1,1 +1,33 @@
-// your code goes here
+import {Item} from './Item';
+
+export abstract class Weapon extends Item {
+  private baseDamage: number;
+  private damageModifier: number;
+  private baseDurability: number;
+  private durabilityModifier: number;
+  private MODIFIER_CHANGE_RATE: number;
+
+  constructor() {
+    super();
+  }
+
+  public getDamage() {
+    return this.baseDamage + this.damageModifier;
+  }
+
+  public getDurability() {
+    return (this.baseDurability + this.durabilityModifier) * 100;
+  }
+
+  toString() {
+    return `${super.toString()}, Damage: ${this.formatNumber(
+      this.getDamage()
+    )}, Durability: ${this.formatNumber(this.getDurability())}%`;
+  }
+
+  public use() {
+    return `You use the ${this.getName} , dealing ${this.formatNumber(
+      this.getDamage()
+    )} points of damage.`;
+  }
+}
