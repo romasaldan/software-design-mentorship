392a440b8e1cd639314a9ccbd59c3f7451e096c5
Roman Saldan <Roman_Saldan@epam.com>
2021-06-20 11:21:02 +0300

    trigger

--- 07_algs/index.ts
+++ 07_algs/index.ts
@@ -4,6 +4,7 @@ import {QComparator} from './src/QComparator';
 import {QElement} from './src/QElement';
 
 const jobRunner = new JobRunner(new MaxHeap<QElement>(new QComparator()));
+
 for (let i = 0; i < 10000; i++) {
     const priority = Math.floor(Math.random() * 10000);
     const callback = () => console.log(i, priority);
